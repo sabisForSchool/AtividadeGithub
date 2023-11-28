38a74e1a19f8b8411b082a86fca0eef105fc0e31
JessycaTaynara <jessycatay16@gmail.com>
2023-11-28 17:09:20 -0300

    html base

---
+++ Atividade 1/src/code/index.js
@@ -0,0 +1,11 @@
+class Calculadora{
+    soma(n1, n2){
+        return n1+n2
+    }
+    subtracao(n1, n2){
+        if(n1>n2){
+            return n1-n2
+        }
+        return n2-n1
+    }
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 <h1>Alunas</h1>
 <p>Sabrina Bezerra - 20201214010012</p>
+<p>Jéssyca Taynara - 20201214010034</p>
