56c088b60f442988a152c09a28a889f7cfd68c23
sabiss <sabrinabezsilva@gmail.com>
2023-11-28 17:39:19 -0300

    função de calcular

--- Atividade 1/src/code/index.js
+++ Atividade 1/src/code/index.js
@@ -8,4 +8,38 @@ class Calculadora{
         }
         return n2-n1
     }
+    multiplicacao(numero1, numero2){
+        return numero1*numero2
+    }
+    divisao(numero1, numero2){
+        if(numero1>numero2){
+            return numero1/numero2
+        }
+        return numero2/numero1
+    }
+}
+const nossaCalculadora = new Calculadora()
+function calcular(){
+    const numero1 = parseInt(document.querySelector('input#num1').value)
+    const numero2 = parseInt(document.querySelector('input#num2').value)
+    const operacao = document.querySelector('select#operadores').value
+    const valorResultado = document.querySelector('div#resultado')
+    let resultado;
+
+    switch(operacao){
+        case 'soma':
+            resultado = nossaCalculadora.soma(numero1, numero2)
+            break
+        case 'subtracao':
+            resultado = nossaCalculadora.subtracao(numero1, numero2)
+            break
+        case 'multiplicacao':
+            resultado = nossaCalculadora.multiplicacao(numero1, numero2)
+            break
+        case 'divisao':
+            resultado = nossaCalculadora.divisao(numero1, numero2)
+            break
+    }
+
+    valorResultado.innerHTML = resultado
 }
